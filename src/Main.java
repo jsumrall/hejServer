4216b449dd334b0e50b59e284560b224c9ed64a6
Jonathan Sumrall <jmsumrall@gmail.com>
2014-06-24 16:33:57 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
+import java.io.Console;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  * Created by max on 6/23/14.
@@ -11,23 +13,26 @@ import java.net.UnknownHostException;
 public class Main {
 
     public static void main(String[] args){
+        int port = 9000;
         DatabaseUtils dbUtil = new DatabaseUtils();
-        ThreadPooledServer server = new ThreadPooledServer(9000, dbUtil);
+        ThreadPooledServer server = new ThreadPooledServer(port, dbUtil);
         Thread t = new Thread(server);
         t.start();
 
-
+        Scanner reader = new Scanner(System.in);
         //DBCollection table = db.getCollection("user");
+        System.out.println("Server Started on port: " + port);
 
-
-
-        try {
-            Thread.sleep(50 * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        String clInput = "";
+        while(!clInput.equals("stop")){
+            clInput = reader.next().trim().toLowerCase();
+            if(clInput.equals("stop")){
+                System.out.println("Stopping Server");
+                server.stop();
+            }
         }
-        System.out.println("Stopping Server");
-        server.stop();
+
+        //server.stop();
     }
 }
 
