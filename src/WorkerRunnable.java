7b8270a54aec99e2646619021538002d184a38f1
Jonathan Sumrall <jmsumrall@gmail.com>
2014-06-25 11:00:22 +0200

    validate username , and check if username exists in DB

--- src/WorkerRunnable.java
+++ src/WorkerRunnable.java
@@ -35,7 +35,7 @@ public class WorkerRunnable implements Runnable{
             String[] request;
             request = BR.readLine().split(",");
             System.out.println("Request Received");
-            System.out.println(request[0]);
+            System.out.println(request);
             if(request.length < 2){
                 output.write(new String("Malformed Request: " + request + "\n").getBytes());
                 return; // this feels wrong. I want to return and kill this thread.
